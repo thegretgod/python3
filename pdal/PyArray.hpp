a7ea46e65987d53096c626d65f109de54512a74b
thegretgod <92670569+thegretgod@users.noreply.github.com>
2023-10-01 19:21:21 +0530

    Update PyArray.hpp 3.0

--- pdal/PyArray.hpp
+++ pdal/PyArray.hpp
@@ -90,6 +90,40 @@ private:
     npy_intp *m_stride;
     bool m_done;
 };
+# Define calculator functions
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+# User input
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Perform the calculation
+if operator == "+":
+    result = add(num1, num2)
+elif operator == "-":
+    result = subtract(num1, num2)
+elif operator == "*":
+    result = multiply(num1, num2)
+elif operator == "/":
+    result = divide(num1, num2)
+else:
+    result = "Invalid operator"
+
+print(f"Result: {result}")
+
 
 } // namespace python
 } // namespace pdal
